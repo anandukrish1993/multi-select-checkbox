09d37ecb38d4d0bc6b06ebcd5ae3366be3bde0ef
Anandukrishnan P S <anandukrishnan.ps@ADRAPIDVALUE.COM>
2018-03-15 19:44:18 +0530

    table structure setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tableHeaders: object[] = [
+    { header: "Name" }, { header: "Nation" }, { header: "Franchise" }, { header: "Field Position" }
+  ]
+  tableData: object[] = [];
+  inputModels: object = {
+    name: '', nationality: '', Franchise: '', position: ''
+  }
+  submitClick(): void {
+    if (this.inputModels.name) {
+      this.tableData.push(this.inputModels);
+      this.inputModels = { name: '', nationality: '', Franchise: '', position: '' };
+    }
+  }
+  cancelClick(): void {
+    this.inputModels = { name: '', nationality: '', Franchise: '', position: '' };
+  }
 }
